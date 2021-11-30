9ef4ef1b43105182f726cf22cd5560ca412885c6
Yeon <80025908+JeeYeon210304@users.noreply.github.com>
2021-11-30 16:19:18 +0900

    Yesterday's code
    I missed it. So commit today.

---
+++ JustForStudy/src/JustForStudy_02/DoIt_JAVA.java
@@ -0,0 +1,19 @@
+package JustForStudy_02;
+
+public class DoIt_JAVA{
+
+	public static void main(String[] args) {
+		print01();
+		nonStaticMethod print02 = new nonStaticMethod();
+	}
+	
+	public static void print01() {
+		System.out.println("test");
+	}
+	
+	public void nonStaticMethod() {
+		
+	}
+	
+	
+}
